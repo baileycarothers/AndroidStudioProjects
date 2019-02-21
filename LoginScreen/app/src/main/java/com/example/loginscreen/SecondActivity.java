d3397169a66ce73bb6e85458b69dc4e6804c216f
baileycarothers <44585056+baileycarothers@users.noreply.github.com>
2019-02-21 02:13:16 -0600

    Finished Second Activity

--- LoginScreen/app/src/main/java/com/example/loginscreen/SecondActivity.java
+++ LoginScreen/app/src/main/java/com/example/loginscreen/SecondActivity.java
@@ -3,36 +3,61 @@ package com.example.loginscreen;
 import android.support.v7.app.AppCompatActivity;
 import java.util.Random;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
-    private TextView int1;
-    private TextView int2;
-    private TextView incorrect;
-    private EditText answer;
-    private Button submit;
-    private int IncorrectAttempts = 0;
+    private TextView Info;
+    private int Counter = 0;
+    private Button Submit;
+    private TextView MathProblem;
+    private EditText Answer;
+    private TextView ShowAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        Random rand = new Random();
-        int rand_int1 = rand.nextInt(100) + 1;
-        int rand_int2 = rand.nextInt(100) + 1;
-        int randAnswer = rand_int1 + rand_int2;
-
-        int1 = (TextView)findViewById(R.id.num1);
-        int2 = (TextView)findViewById(R.id.num2);
-        answer = (EditText)findViewById(R.id.etAnswer);
-        submit = (Button)findViewById(R.id.btnSubmit);
-        incorrect = (TextView)findViewById(R.id.tvIncorrect);
-
-        incorrect.setText("Number of Incorrect Answers: " + IncorrectAttempts);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Random rand = new Random();
+        int rand_int1 = rand.nextInt(1000) + 1;
+        int rand_int2 = rand.nextInt(1000) + 1;
+        final int randAnswer = rand_int1 + rand_int2;
+
+        ShowAnswer = (TextView) findViewById(R.id.showAnswer);
+        MathProblem = (TextView) findViewById(R.id.mathProblem);
+        Answer = (EditText) findViewById(R.id.etAnswer);
+        Submit = (Button) findViewById(R.id.btnSubmit);
+        Info = (TextView) findViewById(R.id.incorrectAttempts);
+
+        ShowAnswer.setText("Answer: " + randAnswer);
+        Info.setText("Number of Incorrect Answers: " + String.valueOf(Counter));
+        MathProblem.setText(rand_int1 + " + " + rand_int2);
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Answer.getText().toString(), randAnswer);
+            }
+        });
+    }
+
+    private void validate(String userAnswer, int randAnswer) {
+        int userNum = Integer.parseInt(userAnswer);
+        if (userNum == randAnswer)
+        {
+            Info.setText("Congrats you can do math!");
+        }
+        else {
+            Counter++;
+            Info.setText("Number of Incorrect Answers: " + String.valueOf(Counter));
+            if (Counter == 2) {
+                Info.setText("You suck at math");
+                Submit.setEnabled(false);
+            }
+        }
     }
 }
